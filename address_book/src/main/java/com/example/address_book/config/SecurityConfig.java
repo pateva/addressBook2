6607f5abdd8e3d2f8fd08873429a152c551dfe26
Petya Ateva <petya.ateva@cleverpine.com>
2024-02-19 22:48:27 +0200

    add security config

---
+++ address_book/src/main/java/com/example/address_book/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.example.address_book.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests(authorize -> authorize
+                        .anyRequest().authenticated())
+                .cors(withDefaults())
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(withDefaults())
+                );
+        return http.build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return JwtDecoders.fromIssuerLocation("https://dev-ujeedw6anz18yzxk.us.auth0.com/");
+    }
+}
