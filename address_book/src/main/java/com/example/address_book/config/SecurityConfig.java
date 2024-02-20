cf6a82d1153cb54a9d4da3c4ec2b8b67662214c5
Petya Ateva <petya.ateva@cleverpine.com>
2024-02-20 13:21:30 +0200

    update jwt decoder

--- address_book/src/main/java/com/example/address_book/config/SecurityConfig.java
+++ address_book/src/main/java/com/example/address_book/config/SecurityConfig.java
@@ -1,18 +1,19 @@
 package com.example.address_book.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -27,6 +28,8 @@ public class SecurityConfig {
 
     @Bean
     public JwtDecoder jwtDecoder() {
-        return JwtDecoders.fromIssuerLocation("https://dev-ujeedw6anz18yzxk.us.auth0.com/");
+
+        return NimbusJwtDecoder.withJwkSetUri(System.getenv("JWK_SET")).build();
     }
+
 }
