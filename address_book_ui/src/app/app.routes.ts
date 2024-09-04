736109677c5043e0a782bd5387881a2e259b159d
Petya Ateva <petya.ateva@cleverpine.com>
2024-09-04 14:21:27 +0300

    add assets folder

--- address_book_ui/src/app/app.routes.ts
+++ address_book_ui/src/app/app.routes.ts
@@ -2,15 +2,22 @@ import { Routes } from '@angular/router';
 import { RecordComponent } from './record/record.component';
 import { TestComponent } from './test/test.component';
 
+const homePath = '';
+const recordsPath = 'records';
+
 export const routes: Routes = [
     {
-        path: 'test',
+        path: homePath,
         component: TestComponent,
-        title: "Placeholder 2"
+        title: "Home"
     },
     {
-        path: 'records',
+        path: recordsPath,
         component: RecordComponent,
         title: "Record"
+    },
+    {
+        path: '**',
+        redirectTo: homePath
     }
 ];
