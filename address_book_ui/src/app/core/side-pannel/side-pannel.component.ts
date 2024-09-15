b32fa3b9acf0c02542a1d31c2a788e82395640f4
Petya Ateva <petya.ateva@cleverpine.com>
2024-09-15 17:43:04 +0300

    add tree class and a button footer in the side panel

--- address_book_ui/src/app/core/side-pannel/side-pannel.component.ts
+++ address_book_ui/src/app/core/side-pannel/side-pannel.component.ts
@@ -1,14 +1,36 @@
 import { Component } from '@angular/core';
 import { SidebarModule } from 'primeng/sidebar';
 import { ButtonModule } from 'primeng/button';
+import { TreeModule } from 'primeng/tree';
+import { TreeNode } from 'primeng/api';  
+
+
 
 @Component({
   selector: 'app-side-pannel',
   standalone: true,
-  imports: [SidebarModule, ButtonModule],
+  imports: [SidebarModule, ButtonModule, TreeModule],
   templateUrl: './side-pannel.component.html',
   styleUrl: './side-pannel.component.scss'
 })
 export class SidePannelComponent {
+
 sidebarVisible: boolean = true;
+files: TreeNode[] = [
+  {
+    label: 'My Account',
+    icon: 'pi pi-user', // Icon for profile
+    // Define an action or a command later for navigation or opening panels
+  },
+  {
+    label: 'Contacts',
+    icon: 'pi pi-users',  // Icon for contacts
+    // Define action for contacts here
+  },
+  {
+    label: 'Labels',
+    icon: 'pi pi-tag', // Icon for labels
+    // Define action for labels here
+  }
+];
 }
