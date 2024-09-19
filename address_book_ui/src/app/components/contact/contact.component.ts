bb17ec701320f3093c663bd0d336856dc9004a50
Petya Ateva <petya.ateva@cleverpine.com>
2024-09-19 22:24:26 +0300

    add an update overlay for the profile pic

--- address_book_ui/src/app/components/contact/contact.component.ts
+++ address_book_ui/src/app/components/contact/contact.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
+import { BadgeModule } from 'primeng/badge';
+
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CardModule, ButtonModule],
+  imports: [CardModule, ButtonModule, BadgeModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
-
+  header: string = "First Name";
+  subheader: string = "+12345678"
 }
