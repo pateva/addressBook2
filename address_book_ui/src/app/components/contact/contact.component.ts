e6d759ce676b5c039ca3016f6d2694d9dc2a4951
Petya Ateva <petya.ateva@cleverpine.com>
2024-09-20 22:53:56 +0300

    add more buttons, which do not work as they should

--- address_book_ui/src/app/components/contact/contact.component.ts
+++ address_book_ui/src/app/components/contact/contact.component.ts
@@ -4,8 +4,12 @@ import { ButtonModule } from 'primeng/button';
 import { BadgeModule } from 'primeng/badge';
 import { InputTextModule } from 'primeng/inputtext';
 import { FormsModule } from '@angular/forms';
-import { NgFor, NgIf} from '@angular/common';
+import { NgFor, NgIf, AsyncPipe } from '@angular/common';
 import { TableModule } from 'primeng/table';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { ChangeDetectorRef } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
 
 @Component({
   selector: 'app-contact',
@@ -17,32 +21,42 @@ import { TableModule } from 'primeng/table';
     FormsModule,
     NgFor,
     NgIf,
-    TableModule],
+    AsyncPipe,
+    TableModule,
+    InputGroupModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+  constructor(private cdr: ChangeDetectorRef) {}
+
   header: string = "First Name";
   subheader: string = "+12345678";
   streetAddress: string = '';  // To store street/precise address
   location: string = '';  // To store location
- 
+
   contactDetails = [
+    { type: 'Phone Number', value: '+359876616112', isDisabled$: new BehaviorSubject<boolean>(true) },
+    { type: 'Email', value: 'john.doe@example.com', isDisabled$: new BehaviorSubject<boolean>(true) },
+  ];
+  address = [
     { type: 'Phone Number', value: '+359876616112' },
     { type: 'Email', value: 'john.doe@example.com' },
   ];
-  address = [
-    
-  ];
-  isDisabledAddress: boolean = true;
   isDisabled = true;
 
 
   enableTextFields() {
-    this.isDisabledAddress = false;
+    this.isDisabled = false;
   }
 
-  saveAddress() {
-    this.isDisabledAddress = true;
+  saveAddress(index: number) {
+    this.contactDetails[index].isDisabled$.next(true);
+    // this.cdr.detectChanges(); // Force change detection
+  }
+  
+  updateAddress(index: number) {
+    this.contactDetails[index].isDisabled$.next(true);
+    // this.cdr.detectChanges(); // Force change detection
   }
 }
